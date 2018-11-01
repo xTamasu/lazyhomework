030875a2a04c266a8dadf972485fc31ee9e2fd5b
ItzNitroX <nicogreffin@gmail.com>
2018-11-01 18:56:09 +0100

    added register.php
    Register Seite erstellt

---
+++ register.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+
+     <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <body class="text-center" background="img/loginbg.png" width="200">
+      <form class="form-signin">
+        <img class="mb-4" src="img/lazyHomework_logo_dunkel.png" alt="" width="300">
+        <h1 class="h3 mb-3 font-weight-normal">Registrierung</h1>--
+        <label for="inputEmail" class="sr-only">Email Adresse</label>
+        <input type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Passwort</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Passwort" required>
+        <label for="inputPasswordVerify" class="sr-only">Passwort bestätigen</label>
+        <input type="password" id="inputPasswordVerify" class="form-control" placeholder="Passwort bestätigen" required>
+        <div class="checkbox mb-3">
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onclick="window.location.href='index.php'">Registrieren</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onclick="window.location.href='index.php'">Doch lieber anmelden</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+      </form>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
