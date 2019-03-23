b9230190b83a16c1bbdecc8960f916648df5747e
Tamasu <Lukasklepper@hotmail.de>
2019-03-23 22:16:41 +0100

    fixed loginLogic
    fixed redirect

--- loginLogic.php
+++ loginLogic.php
@@ -6,42 +6,45 @@ include ("connector.php");
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+
 if (isset($username) && isset($password))
-{
+{	
+	
 	$sql = "SELECT Benutzername, Passwort FROM User WHERE Benutzername='" .$username. "'";
 	foreach ($conn->query($sql) as $row) 
 	{
 		$row['Benutzername']." ".$row['Passwort']."<br />";
 		$row['Benutzername']."<br /><br />";
 	}
-
+	
 	if (@$row['Benutzername'] == $username && $row['Passwort'] == $password)
 	{
+		
+		
 		$Sessioito = session_id();
 		//echo 'Hello ' . htmlspecialchars($_COOKIE["name"]) . '!';
 		//SQL 
-		$pdo = new PDO("mysql:dbname=lazyhomework;host=127.0.0.1", "root", "");
+		$pdo = new PDO("mysql:dbname=lazyhomework;host=localhost", "lazyhomework", "lazyboys"); //In Lokalen Test Versionen muss "host=127.0.0.1" sonst gehts nicht
  
-		$statement = $pdo->prepare("UPDATE `user` SET `SessionID` = '$Sessioito' WHERE `user`.`Benutzername` = '$username'");
+		$statement = $pdo->prepare("UPDATE `User` SET `SessionID` = '$Sessioito' WHERE `User`.`Benutzername` = '$username'");
+		
 		$statement->execute(array('SessionID' => '567'));
 		
 		
-		
  		
-		header("Location: /lazyhomework/home.php");
-		
+		header("Location:home.php");
+		//echo (session_id());
 		//exit();
-	}
-	else 
-	{
-		$e = "wp";
-		//alsches Passwort oder Username";
 		
+	}
+	else // Falsches Passwort oder Benutzername "wp"
+	{
+		header("Location:index.php?e=wp");
 	}
 }
-else 
+else // Keine Eingabe "ne"
 {
-	//$e = "ne";
-	header("echo 2");
+	header("Location:index.php?e=ne");
+	
 }
 ?>
