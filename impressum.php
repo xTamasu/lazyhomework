58a223797bd219803662e2b35789df1f09068f21
ItzNitroX <nicogreffin@gmail.com>
2019-03-24 19:24:08 +0100

    Create impressum.php

---
+++ impressum.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+
+<head>
+<link rel="shortcut icon" href="img/lazyHomework_favicon.png" type="image/png" />
+	<meta charset="utf-8">
+	<title>LH | Impressum</title>
+</head>
+
+<body>
+	<?php include ("header.php"); ?>
+
+	<main role="main" class="container">
+		<div style="display:none;" class="showPage animate-bottom">
+      <div id="impressum">
+      </div>
+      
+      
+      
+		</div>
+	</main>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
