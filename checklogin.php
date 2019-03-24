908de0a92e2ff8e67295bfe1197cf581e5f6757a
Tamasu <Lukasklepper@hotmail.de>
2019-03-24 18:52:14 +0100

    added SettingsCheck

--- checklogin.php
+++ checklogin.php
@@ -4,15 +4,20 @@ session_start();
 include("connector.php");
 
 if ($result = $conn->query("SELECT SessionID FROM User WHERE SessionID ='" .session_id(). "'")) {
-
     /* determine number of rows result set */
 	$row_cnt = $result->num_rows;
-	
 	if($row_cnt < 1)
 	{
 		header("Location:index.php");
 	}
+
+	foreach ($conn->query("SELECT Gruppe, SessionID FROM User WHERE SessionID ='" .session_id(). "'") as $row) 
+	{
+
+	}
+	$gruppe = $row['Gruppe'];
+	$sessionid = $row['SessionID'];
     /* close result set */
-    $result->close();
+	$result->close();
 }
 ?>
