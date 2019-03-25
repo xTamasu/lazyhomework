406bb333eb716fa91d699d430cb47c57c5177411
Tamasu <Lukasklepper@hotmail.de>
2019-03-25 21:00:43 +0100

    Update getUserInformation.php

--- getUserInformation.php
+++ getUserInformation.php
@@ -1,10 +1,11 @@
 <?php
 foreach($conn->query
 (
-"SELECT User.SessionID, Gruppen.* FROM `User` JOIN Gruppen ON User.GruppeID = Gruppen.gruppe_ID WHERE SessionID = '" . session_id() . "'"
+"SELECT User.SessionID, User.Benutzername, Gruppen.* FROM `User` JOIN Gruppen ON User.GruppeID = Gruppen.gruppe_ID WHERE SessionID = '" . session_id() . "'"
 ) as $userinformation)
 {
 	$sessionid = $userinformation['SessionID'];
+	$username = $userinformation['Benutzername'];
 	$gruppe = $userinformation['gruppe_ID'];
 	$gruppeName = $userinformation['name'];
 	$canEditHomework = $userinformation['canEditHomework'];
